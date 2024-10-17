27dbc6ac89c0082cb7e92a374c78b4e925d6e165
apoorv mittal <apoorv.mittal92@gmail.com>
2024-10-17 22:34:08 +0530

    ADDED CRUD

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns=[
     path("save",views.save),
-    path("",views.read)
+    path("",views.read),
+    path("update/<int:id>",views.update),
+    path("delete/<int:id>",views.delete)
 ]
--- app/views.py
+++ app/views.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import User
 from .models import User
 import json
 
-# Create your views here.
+'''
+Create
+'''
 @api_view(['POST'])
 def save(request):
     body = json.loads(request.body.decode("utf-8"))
@@ -17,6 +19,9 @@ def save(request):
         )
     return JsonResponse(body)
 
+'''
+Read
+'''
 @api_view(['GET'])
 def read(self):
     allUsers =list(User.objects.values()) 
@@ -24,3 +29,33 @@ def read(self):
         'users':allUsers
     })
 
+'''
+Update
+'''
+@api_view(['PUT'])
+def update(request,id):
+    body = json.loads(request.body.decode("utf-8"))
+    print("id",id)
+    find = User.objects.filter(id=id)
+    if find:
+        find.update(
+            firstname=body.get('firstname'),
+            lastname=body.get('lastname'),
+            phoneNo=body.get('phoneNo'),
+            )
+        return JsonResponse({
+            'message':'Updated',
+            'find':tuple(find.values())
+        })
+    else:
+        return JsonResponse({
+            'message':'Not found',
+        },status=404)
+
+'''
+Delete
+'''
+@api_view(['DELETE'])
+def delete(request,id):
+    User.objects.filter(id=id).delete()
+    return JsonResponse({  'message':'Deleted',  },status=200)

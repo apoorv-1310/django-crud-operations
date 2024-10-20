f90d28b321b259c95e188326d0ea6d17aef8eb54
apoorv mittal <apoorv.mittal92@gmail.com>
2024-10-20 12:51:02 +0530

    added middlewares

---
+++ app/middleware/main.py
@@ -0,0 +1,20 @@
+from django.http import JsonResponse
+
+
+class CustomMiddleware:
+
+    def __init__(self,get_response):
+        response = self.get_response = get_response  
+
+    def __call__(self,request):
+        print("Token",request.META.get('HTTP_AUTHORIZATION'))
+        token = request.META.get('HTTP_AUTHORIZATION')
+        if(token):
+            response = self.get_response(request)  
+            return response
+        else:
+            return JsonResponse({
+                'message':'Unauthorized'
+            },status=401)
+
+      
